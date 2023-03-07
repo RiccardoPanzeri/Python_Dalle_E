71262d99ffd6cce7747420405d5899ec1fa8cf48
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2023-03-07 02:08:06 +0100

    Modifica al file principale

--- Python_Dalle_E.py
+++ Python_Dalle_E.py
@@ -1,25 +1,24 @@
+# create.py
 
-
-
+import os
 
 import openai
 restart = True
 risposta = '/'
 
 while restart:
-    #il prompt contiene la stringa che rappresenta la richiesta fatta a DALL-E sull'immagine da generare;
+    #il prompt contiene la stringa che rappresenta la richiesta fatta a DALL-E sull'immagine da generare
     promptUtente = input("inserisci qualcosa che vorresti vedere realizzato come immagine\n")
-    #Chiave API OpenAI personale
-    openai.api_key = "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"
 
-    response = openai.Image.create(#genero l'immagine;
+    openai.api_key = "Inserisci qui la tua API Key"
+
+    response = openai.Image.create(
         prompt=promptUtente,
-        n=1,#setto il numero di immagini da creare a 1;
-        size="1024x1024",#dimensioni immagine;
+        n=1,
+        size="1024x1024",
     )
 
-    print(response["data"][0]["url"])#genero il link URL che, se aperto tramite browser, mostrerà l'immagine generata;
-    #permetto all'utente di generare altre immagini
+    print(response["data"][0]["url"])
     risposta = input("Vuoi generare un'altra immagine? (y- si, qualunque altro tasto per uscire)")
     if risposta == 'y':
         restart = True
